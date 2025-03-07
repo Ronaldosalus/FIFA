e70d0b0d54aac9e50ed03746e151a76db0507191
Ronaldosalus <ronaldotrinsalus@gmail.com>
2025-03-07 17:45:08 -0300

    Update 1_Home.py

--- FIFA23/1_Home.py
+++ FIFA23/1_Home.py
@@ -16,7 +16,7 @@ st.markdown('''
 ''')
 
 if 'data' not in st.session_state:
-    df_data = pd.read_csv("datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = pd.read_csv("FIFA23/datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
     df_data = df_data[df_data['Contract Valid Until'] >= datetime.today().year]
     st.session_state['data'] = df_data
 
